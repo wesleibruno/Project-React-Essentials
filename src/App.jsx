ae361aa829dfcc2927b19cda000b0bbb20487e94
Weslei Bruno <weslei945@gmail.com>
2024-04-02 23:41:14 -0300

    Separate File Header and CoreConcept

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-function Header() {
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header.jsx";
+import { CORE_CONCEPTS } from "./data.js";
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
